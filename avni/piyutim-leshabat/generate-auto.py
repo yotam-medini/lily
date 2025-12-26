34bc67bed41bb2a795981acd4e9d0f9b30145dd7
Yotam Medini <yotam.medini@gmail.com>
2025-12-26 17:37:40 +0200

    Avni/Piyutim restore generate_single reduced

--- avni/piyutim-leshabat/generate-auto.py
+++ avni/piyutim-leshabat/generate-auto.py
@@ -23,6 +23,40 @@ def strnow():
 def get_autogen_header() :
     return f"% Auto-generated on {strnow()} by {sys.argv[0]}. Do NOT edit"
 
+def generate_single(lilypond_version, name):
+    autogen_header = get_autogen_header() 
+    for lang in ["hebrew", "latin", "hebrewlatin"]:
+        os.makedirs(lang, exist_ok=True)
+
+        f = open(f"{lang}/{name}-lyrics-context.ly", "w")
+        f.write(f"{autogen_header}\n")
+        for actual_lang in ["hebrew", "latin"]:
+            if actual_lang in lang:
+                f.write(f'\\include "{name}-{actual_lang}-ctx.ly"\n')
+        f.close()
+        
+        for voice in VOICES:
+            f = open(f"{lang}/{name}_{voice}lyrics.ly", "w")
+            f.write(f"{autogen_header}\n")
+            for actual_lang in ["hebrew", "latin"]:
+                if actual_lang in lang:
+                    lname = f"{name}_{voice}_lyrics_{actual_lang}"
+                    f.write(f'\\new Lyrics = "{lname}"\n')
+            f.close()
+
+    for lang in ["hebrew", "latin"]:
+        f = open(f"{name}-{lang}-ctx.ly", "w")
+        f.write(f"{autogen_header}\n")
+        indent = 8*' '
+        for voice in VOICES:
+            f.write(
+            f'{indent}\\context Lyrics = "{name}_{voice}_lyrics_{lang}" {LB}\n'
+            f'{indent}  \\lyricsto "{name}_{voice}" {LB}\n'
+            f'{indent}    \\{name}_{voice}_{lang}_lyrics_text\n'
+            f'{indent}  {RB}\n'
+            f'{indent}{RB}\n')
+        f.close()
+
 def generate_single_midi(lilypond_version, name):
     autogen_header = get_autogen_header() 
     f = open(f"{name}-midi.ly", "w")
@@ -110,6 +144,7 @@ def generate_body(names):
 
 def generate_for_names(lilypond_version, names):
     for name in names:
+        generate_single(lilypond_version, name)
         generate_single_midi(lilypond_version, name)
     generate_books(lilypond_version, names)
     generate_body(names)
